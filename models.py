8cbda5ac95e6752a346bce422575198b4e1284a8
rajafarel <rajafarel2004@gmail.com>
2025-02-24 10:57:33 +0700

    Created Document and Signature Table

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey, UUID, Boolean
 
 Base = declarative_base()
 
+# User class
 class User(Base):
     __tablename__ = "user"
     
@@ -17,7 +18,10 @@ class User(Base):
     UserRoleID = Column(Integer(), ForeignKey("userrole.UserRoleID"))
     
     userrole = relationship("UserRole", back_populates="user")
+    documents = relationship("Documents",back_populates="documents")
+    signature = relationship("Signature",back_populates="signature",uselist=False)
     
+# UserRole class
 class UserRole(Base):
     __tablename__ = "userrole" 
     
@@ -26,4 +30,33 @@ class UserRole(Base):
     
     # Back-reference to user
     user = relationship("User", back_populates="userrole")
+
+#Documents class
+class Documents(Base):
+    __tablename__ = "document"
+
+    DocumentID = Column(Integer(),primary_key=True)
+    FileName = Column(String(100))
+    Filetype = Column(String(20))
+    StoragePath = Column(String(50))
+    Status = Column(String(20))
+    CreatedAt = Column(DateTime)
+    SignedAt = Column(DateTime)
+    UserID = Column(Integer(),ForeignKey("user.UserID"))
+
+    #Relationship to user 
+    user = relationship("User",back_populates="user")
+
+#Signature class
+class Signature(Base):
+    __tablename__ = "signature"
+
+    SignatureID = Column(Integer(),primary_key=True)
+    SignatureData = Column(String(100))
+    ExpiryDate = Column(DateTime)
+    UserID = Column(Integer(),ForeignKey("user.UserID"),unique=True)
+
+
+    user = relationship("User",back_populates="user")
+
     
